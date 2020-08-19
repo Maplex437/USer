c89d918b2416903a95cc18150e7378d2ee273857
Maple <18795881315@163.com>
2020-08-19 12:31:29 +0800

    0819

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="demo">
+      <li v-show="i===activeIndex" :v-for="(item,i) in imgList">
+        <a href="#">
+          <img :src="imgUrl" style="width:300px;height:170px;">
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
-
+ <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 不加载提示
+Vue.config.devtools = true;
+Vue.config.productionTip = false;
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+let vm = new Vue({
+  el: "#app",
+  data() {
+    return {
+      activeIndex: 0,
+      imgList: [
+        "@/assets/ninja640.jpg",
+        "@/assets/ninja392.jpg",
+        "@/assets/ninja854.jpeg",
+      ],
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      if (++this.activeIndex === this.imgList.length) this.activeIndex = 0;
+    }, 1000);
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="summary">
+    <div class="gallery">
+      <div class="product-img">
+        <div
+          @click="prevImg"
+          class="prev-arrow"
+          :class="{'prev-active': galleryList.length - 3 <= 0 || this.offsetCount < 1}"
+        >
+          <img src="../assets/images/productDetail/left.png" alt="左箭头" />
+        </div>
+        <ul>
+          <li class="list-img">
+            <div class="list-img-wrap" ref="carouser">
+              <div
+                @click="getCurImg(item.ImageCode, index)"
+                v-for="(item,index) in galleryList"
+                :key="index"
+                class="single-img"
+                :class="{'img-activce': item.ImageCode == curCode && imgActive}"
+              >
+                <img :src="item.ImageCode" alt="缩略图" />
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div
+          @click="nextImg"
+          class="next-arrow"
+          :class="{'next-active': galleryList.length - 3 <= 0 || offsetCount >= galleryList.length - 3 }"
+        >
+          <img src="../assets/images/productDetail/right.png" alt="右箭头" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style lang="scss" scoped>
+@import "World.scss"
+</style>>
